5f8c44bb592521237a4c011ffa12120630dca0f3
sandySwap <sandy.androit@gmail.com>
2016-09-11 17:03:42 +0700

    versi 1.0.0

---
+++ src/main/java/com/coba/LathibernateApplication.java
@@ -0,0 +1,12 @@
+package com.coba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LathibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LathibernateApplication.class, args);
+	}
+}
