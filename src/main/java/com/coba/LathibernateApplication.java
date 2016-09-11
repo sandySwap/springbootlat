03cb8371bb46f24f09afbd2a6465d467e74d10be
sandySwap <sandy.androit@gmail.com>
2016-09-11 17:35:08 +0700

    Test

--- src/main/java/com/coba/LathibernateApplication.java
+++ src/main/java/com/coba/LathibernateApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LathibernateApplication {
 
 	public static void main(String[] args) {
+		System.out.println("coba dit");
 		SpringApplication.run(LathibernateApplication.class, args);
 	}
 }
